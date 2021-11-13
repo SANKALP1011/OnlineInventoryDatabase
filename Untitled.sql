343fe7abaab149a6f063637f4fc0d1c0867cdf1f
SANKALP1011 <tigerishan426@gmail.com>
2021-11-13 06:36:44 +0530

    Query implementation started , stared working on easy queries.

--- Untitled.sql
+++ Untitled.sql
@@ -1,13 +1,31 @@
+/*
+- This is an online shop inventory databse which consists of two tables which are Order and Customer.
+- Tuples in the relations are less because this is just a demo databse to demonstrate the use of sql.
+- This database would consist of almost all the querys.
+- Query would be divided into three categories depending on their difficulty level.
+- Three categories are-:
+  *Easy
+  *Medium
+  *Hard
+- I'll try to cover as many queries as possible so that it would be easy for the user to refer the query for their own use.
+- Souce code would be uploaded on github.
+*/
+
+
+##Creating a database -:
+
 create database OnlineShopInventorydb;
 use  OnlineShopInventorydb;
 
 create table Orders(
-    OrderId int primary key,
+    OrderId int primary key,                   ##Use primary key for the attribute OrderId because orderId would always be unique.
     OrderName varchar(70),
-    OrderDate date,
+    OrderDate date,                            ##MySql provides us with a date function to declare date.
     OrderPrice int
 );
+describe Orders;                               ##Describe Function shows the table to the user.
 
+ 
 insert into Orders
 values(1002,"Phone","2021-04-10",57000),
 (1003,"GameBox","2021-09-16",38000),
@@ -22,7 +40,50 @@ CustName varchar(90),
 CustDob date,
 Address varchar(120),
 OrderId int,
-foreign key(OrderId) references Orders(OrderId)
+foreign key(OrderId) references Orders(OrderId)    ##Refrencing the oookId as foreign key  from Orders(OrderId).  
 );
 
+insert into Customers
+values("John","2021-09-12","Spain",1003),
+("David","2000-09-18","France",1005),
+("Sarah","1995-03-16","New Jersey",1007),
+("Paul","1989-10-1","London",1002),
+("Amy","1992-04-08","Texas",1006)
+;
+select*from customers;
+
+/*
+                                QUERY EXECUTION STARTS FROM HERE-:
+                                       Category - Easy
+*/
+
+##When we run this query then we get all the value from the table Customer because we are using * operation-:
+select* from Customer;  
+select* from Orders;  
+
+##Describe table Customer and Order-:
+describe Customer;
+describe Orders;
+
+/*
+                                     SELECT OPERATIONS-:
+*/
+
+##This query select all the name from the CustomerTable.
+select Custname 
+from Customers;
+##This query selects all the name where address is equal to France from the Customer Table.
+select Custname
+from Customers
+where Address = "France";
+##This query selects all the OrderName along with their price using where condition.
+select OrderName , OrderPrice 
+from Orders 
+where OrderName = "watch"
+and
+OrderPrice = 20000;
+
+
+
+   
 
